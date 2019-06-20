e42f9fd437535094fe90e78d8fd36e9d7f45f6b9
hayj <hay.julien1@gmail.com>
2019-06-20 18:08:57 +0200

    minor updates

--- utils.py
+++ utils.py
@@ -40,7 +40,21 @@ def pairwiseCosineSimilarity(data):
 
 
 def test1():
-	pass
+	# Score random
+	simMatrix = pairwiseCosineSimilarity(vectors)
+	log(pairwiseSimNDCG(simMatrix, labels), logger)
+	for i in range(20):
+	    log(pairwiseSimNDCG(simMatrix, shuffle(labels)), logger)
+	fakeDistMatrix = np.array([[np.nan, np.nan, np.nan, np.nan, np.nan],
+	                      [0.5, np.nan, np.nan, np.nan, np.nan],
+	                      [0.9, 0.8, np.nan, np.nan, np.nan],
+	                      [0.3, 0.5, 0.6, np.nan, np.nan],
+	                      [0.2, 0.1, 0.9, 0.8, np.nan]])
+	fakeSimMatrix = fillTriangleMatrix(fakeDistMatrix)
+	print(ndcg([1, 0, 0, 0, 1, 0, 1]))
+	pairwiseSimNDCG(fakeSimMatrix, ['a', 'a', 'b', 'b', 'b'])
+	pairwiseSimNDCG(fakeSimMatrix, ['a', 'a', 'b', 'b', 'b'])
+	fakeSimMatrix.shape
 
 
 if __name__ == '__main__':
